9ccbeb632f145d18a97d4697a69c554e0b419cf6
Andrew Mason <andrewmasonmedia@gmail.com>
2019-02-09 23:00:02 -0500

    Added swup reinit js for plugins/animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,63 @@ $(document).ready(function() {
 	   }, 500, 'linear');
     });
 });
+
+
+
+//lazy load
+
+$(document).ready(function() {
+    $(".lazy").recliner({
+        attrib: "data-src", // selector for attribute containing the media src
+        throttle: 0,      // millisecond interval at which to process events
+        threshold: 5,     // scroll distance from element before its loaded
+        printable: true    // be printer friendly and show all elements on document print
+    //    live: true          // auto bind lazy loading to ajax loaded elements
+    });
+});
+
+
+//swup reinit plugins
+
+document.addEventListener('swup:contentReplaced', function () {
+    //aos reinit
+    AOS.init();
+
+    //lazyload reinit
+    $(".lazy").recliner({
+        attrib: "data-src", // selector for attribute containing the media src
+        throttle: 0,      // millisecond interval at which to process events
+        threshold: 5,     // scroll distance from element before its loaded
+        printable: true    // be printer friendly and show all elements on document print
+    //    live: true          // auto bind lazy loading to ajax loaded elements
+    });
+
+    //close mobile nav on click
+    $('.nav-link, body').on('click',function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+
+//smooth scroll https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/
+
+    $('a[href*="#"]').on('click', function (e) {
+	   e.preventDefault();
+
+	   $('html, body').animate({
+		  scrollTop: $($(this).attr('href')).offset().top
+	   }, 500, 'linear');
+    });
+
+//lazy load
+    $(".lazy").recliner({
+        attrib: "data-src", // selector for attribute containing the media src
+        throttle: 0,      // millisecond interval at which to process events
+        threshold: 5,     // scroll distance from element before its loaded
+        printable: true    // be printer friendly and show all elements on document print
+    //    live: true          // auto bind lazy loading to ajax loaded elements
+    });
+
+    
+        
+});
+//end swup reinit
+
